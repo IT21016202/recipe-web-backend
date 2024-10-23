1516094a9dde8a9dfc852a652c323934ac7567c2
IT21016202 <it21016202@my.sliit.lk>
2024-10-23 23:01:19 +0530

    modification

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ const cors = require('cors');
 require("dotenv").config();
 const app = express();
 
-// Connect to DB
 connectDB();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Routes
+app.get('/', (req, res) => res.send("API is running"));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/recipes', recipeRoutes);
 
 const PORT = process.env.PORT || 8000;
 
-// Start server
 app.listen(PORT,() => console.log("Server listening at port " + PORT));
 
 
